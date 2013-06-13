81b2b97e3353f209d237b1f764b9c4a1ddd32c41
Henrik <henrik@vinden>
2013-06-14 01:30:10 +0200

    Added functions for information map

--- handy_functions.py
+++ handy_functions.py
@@ -1,7 +1,7 @@
 def format_grouped_images(group_list):
+    '''Make a subj x voxel 2d array for each group'''
     import nibabel as nb
 
-    #Make a subj x voxel 2d array for each group
     def make_matrix(inlist):
         images_4d = nb.funcs.concat_images(inlist)
         sub_data = images_4d.get_data()
@@ -14,3 +14,24 @@ def format_grouped_images(group_list):
         combined_matrix.append(make_matrix(g))
 
     return combined_matrix
+
+def kl_distance(p,q):
+    '''calculates KL-divergence for discrete distributions'''
+    import numpy as np
+
+    p = np.asarray(p, dtype=np.float)
+    q = np.asarray(q, dtype=np.float)
+
+    if ((p==0).sum()+(q==0).sum()) > 0:
+        raise Exception, "Zeros found"
+    kl_dist = np.sum(p * np.log(p/q))
+    return kl_dist
+
+
+def information_map(filelist):
+    '''calculate similarity of signal for each voxel compared to surrounding voxels'''
+    pass
+
+def correlation_map(filelist, labels):
+    '''calculate a correlation map with the labels'''
+    pass
